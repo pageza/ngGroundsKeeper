08a71fcd5c4280879136eea2a9bbece5fb00d4c8
zach <zap.freelance@gmail.com>
2021-07-05 10:17:43 -0600

    Added create-workorder component

--- src/app/create-workorder/create-workorder.component.ts
+++ src/app/create-workorder/create-workorder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-workorder',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-workorder.component.css']
 })
 export class CreateWorkorderComponent implements OnInit {
-
-  constructor() { }
+  school: any
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+    this._route.params.subscribe( params => {
+      this.school = params.school
+    })
   }
-
+  submitWorkorder(){
+    console.log('This will submit the Work Order to the API for validation before sending to the database')
+    // TODO: submit form to API for validation and entry to database
+    this._router.navigate(['/school',this.school])
+  }
 }
