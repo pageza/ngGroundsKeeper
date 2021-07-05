a8197ba8013c48e18fa89e8f4de4271df8853400
zach <zap.freelance@gmail.com>
2021-07-05 09:52:24 -0600

    Added create-workorder component

---
+++ src/app/create-workorder/create-workorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-workorder',
+  templateUrl: './create-workorder.component.html',
+  styleUrls: ['./create-workorder.component.css']
+})
+export class CreateWorkorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
